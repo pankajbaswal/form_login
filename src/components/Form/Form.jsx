650a4a7262c8735112ec13f32e000a11eda75a69
Pankaj Baswal <pankajbaswal3360@gmail.com>
2025-08-14 11:43:41 +0530

    Added useRef logic which doestn't cause re-render

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,19 @@
 // import "./Form.css";
 import isValidEmail from "../../helper/validateEmail";
 import isValidPassword from "../../helper/validatePassword";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 function Form() {
+    const exampleRef =useRef(0);
+
+
+    useEffect(() =>{
+        console.log(exampleRef);
+    } ,[]);
+
+    const [count , setCount] = useState(0);
+
     const [formValues ,setFormValues] =useState({
         email:"",
         password:""
@@ -33,9 +42,16 @@ function Form() {
   return (
     <div>
         New Form
+
+        Count:{count}<br/>
+        ExampleRef :{exampleRef.current}<br/>
+        <button onClick={() => setCount(count+1)}>update Count</button><br/>
+        <button onClick={ () => exampleRef.current++}>update ref</button><br/>
+
       <form onSubmit={handleFormSubmit}>
         <div className="wrapper email-input-wrapper">
           <input 
+               id="email-input"
                type="email" 
                value={formValues.email}
                onChange={(event) =>setFormValues({...formValues , email : event.target.value})}
@@ -43,6 +59,7 @@ function Form() {
         </div>
         <div className="wrapper Password-input-wrapper">
           <input 
+              id="password-input"
               type="password" 
               value={formValues.password}
               onChange={(event) =>setFormValues({...formValues , password : event.target.value})}
